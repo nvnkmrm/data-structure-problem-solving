d09ff321084527c7532f08e49224aa3dfef6cfe5
Naveen Kumar <mnaveensmn@gmail.com>
2026-03-12 06:32:38 +0530

    Refactor sum to target

--- leet-code/string-and-array/04_sum_to_target_sorted_unique_int.py
+++ leet-code/string-and-array/04_sum_to_target_sorted_unique_int.py
@@ -4,15 +4,14 @@
 #
 # For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
 
-def sum_to_target(nums: list[int], target: int) -> tuple[int, int]:
-
+def sum_to_target(nums: list[int], target: int) -> tuple[int, int] | None:
     left = 0
-    right = len(nums)-1
+    right = len(nums) - 1
 
     while left < right:
         sum = nums[left] + nums[right]
         if sum == target:
-            return (nums[left], nums[right])
+            return nums[left], nums[right]
         elif sum < target:
             left += 1
         elif sum > target:
@@ -23,4 +22,3 @@ def sum_to_target(nums: list[int], target: int) -> tuple[int, int]:
 
 if __name__ == "__main__":
     print(sum_to_target([1, 2, 4, 6, 8, 9, 14, 15], 3))
-
