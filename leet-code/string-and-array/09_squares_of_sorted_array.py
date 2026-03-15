6ec477b9dfe00912ef1eb01bc4f79906be8733ec
Naveen Kumar <mnaveensmn@gmail.com>
2026-03-15 15:19:17 +0530

    squares_of_sorted_array

---
+++ leet-code/string-and-array/09_squares_of_sorted_array.py
@@ -0,0 +1,45 @@
+
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+#
+# Example 1:
+#
+# Input: nums = [-4,-1,0,3,10]
+# Output: [0,1,9,16,100]
+# Explanation: After squaring, the array becomes [16,1,0,9,100].
+# After sorting, it becomes [0,1,9,16,100].
+#
+# Example 2:
+#
+# Input: nums = [-7,-3,2,3,11]
+# Output: [4,9,9,49,121]
+#
+# Constraints:
+#
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums is sorted in non-decreasing order.
+#
+# Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+
+def squares_of_sorted_array(nums: list[int]) -> list[int]:
+    l = 0
+    r = len(nums) - 1
+    p = r
+    result = [0] * len(nums)
+
+    while (l <= r):
+
+        if abs(nums[l]) < abs(nums[r]):
+            result[p] = nums[r] * nums[r]
+            r -= 1
+        else:
+            result[p] = nums[l] * nums[l]
+            l += 1
+        p -= 1
+
+    return result
+
+if __name__ == "__main__":
+    print(squares_of_sorted_array([-2,-1,1,2]))
+
