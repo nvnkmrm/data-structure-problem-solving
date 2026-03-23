ea1aca17dc57c8441f11d6849dcf47acb1ec8793
Naveen Kumar <mnaveensmn@gmail.com>
2026-03-23 21:40:28 +0530

    Retried sum to target sorted unique array

--- leet-code/string-and-array/04_sum_to_target_sorted_unique.py
+++ leet-code/string-and-array/04_sum_to_target_sorted_unique.py
@@ -4,21 +4,21 @@
 #
 # For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
 
+# Retried - 1
 def sum_to_target(nums: list[int], target: int) -> tuple[int, int] | None:
     left = 0
-    right = len(nums) - 1
+    right = len(nums)-1
 
     while left < right:
-        sum = nums[left] + nums[right]
-        if sum == target:
-            return nums[left], nums[right]
-        elif sum < target:
+        sum_of_two = nums[left] + nums[right]
+        if sum_of_two == target:
+            return (nums[left], nums[right])
+        elif sum_of_two < target:
             left += 1
-        elif sum > target:
+        else:
             right -= 1
 
-    return None;
-
+    return None
 
 if __name__ == "__main__":
-    print(sum_to_target([1, 2, 4, 6, 8, 9, 14, 15], 3))
+    print(sum_to_target([1, 2, 4, 6, 8, 9, 14, 15], 23))
