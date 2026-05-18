0f214400bd4625174799ca38d284c5a14abe0bed
Naveen Kumar <mnaveensmn@gmail.com>
2026-05-18 06:20:34 +0530

    sum to target un-sorted

---
+++ leet-code/03-hashing/01_sum_to_target_unsorted_duplicates.py
@@ -0,0 +1,55 @@
+# Example 1:
+#
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+#
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+#
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+import pytest
+from typing import Any
+
+
+# 1. Create an empty map.
+# 2. Iterate through the array.
+# 3. For each element num:
+#       Calculate complement = target - num
+#       If complement exists in the map → solution found.
+#       Otherwise store num in the map with its index.
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+# Retried - 1
+def sum_to_target(nums: list[int], target: int) -> list[int] | None:
+    dic = {}
+
+    for i in range(len(nums)):
+        num = nums[i]
+        complement = target - num
+
+        if complement in dic:
+            return [dic[complement], i]
+
+        dic[num] = i
+    return None
+
+
+@pytest.mark.parametrize("nums, target, expected", [
+    ([2, 7, 11, 15], 9, [0, 1]),
+    ([3, 2, 4], 6, [1, 2]),
+    ([3, 3], 6, [0, 1]),
+    ([1, 2, 3, 4, 5], 9, [3, 4]),
+    ([1, 2, 3], 10, None),
+])
+def test_sum_to_target(nums, target, expected):
+    assert sum_to_target(nums, target) == expected
+
+
+if __name__ == '__main__':
+    print(sum_to_target([1, 2, 3, 4, 5], 9))
